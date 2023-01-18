a8694f9f971814679c0fa455ef1f4c69a5462c62
hrytsunyk <serhii.hrytsunyk@gmail.com>
2023-01-18 16:56:33 +0200

    final res

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -4,11 +4,13 @@ const Character = ({character}) => {
     const {id, name, status, species, gender, image} = character;
     return (
         <div className={'character-card'}>
+            <div className={'image'}>
+                <img src={image} alt={name}/>
+            </div>
             <div className={'about'}>
                 <h4>{id}. Name: {name} | Gender: {gender}</h4>
                 <h5>Status: {status} | Species: {species}</h5>
             </div>
-            <div className={'imgage'}><img src={image} alt={name}/></div>
         </div>
     );
 };
