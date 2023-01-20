4f161e5ea09671aeaca5a667e04393403e050016
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-01-20 15:47:31 +0200

    final res hw2

--- src/componets/Users/Users.js
+++ src/componets/Users/Users.js
@@ -9,14 +9,13 @@ import {User} from "../User/User";
 const Users = () => {
 
     const [users, setUsers] = useState([]);
-    const [count, setCount] = useState(0);
 
     const [address, setAddress] = useState(null);
 
 
     useEffect(()=>{
             userServices.getAll().then(user => user.data).then(user => setUsers([...user]))
-        }, [count]);
+        }, []);
 
     return (
 
