32105bc9912968ab333f6ed3209b8fe9c567a548
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-02-06 12:36:09 +0200

    fixed

--- src/components/AnimalsReducer/AnimalsReducer.js
+++ src/components/AnimalsReducer/AnimalsReducer.js
@@ -12,20 +12,20 @@ const reducer = (state, action) => {
         case "ADD_CAT":
             const [lastCat] = state.cats.slice(-1);
             const catsID = lastCat ? lastCat.id + 1 : 1;
-            return{...state, cats:[...state.cats, {id:catsID, name: action.payload}]};
+            return {...state, cats: [...state.cats, {id: catsID, name: action.payload}]};
 
         case "DELETE_CAT":
-            const indexCat = state.cats.indexOf(cat => cat.id === action.payload);
+            const indexCat = state.cats.findIndex(cat => cat.id === action.payload);
             state.cats.splice(indexCat, 1)
             return {...state};
 
         case "ADD_DOG":
             const [lastDog] = state.dogs.slice(-1);
-            const dogsID = lastDog ? lastDog.id +1 : 1;
-            return {...state, dogs:[...state.dogs, {id:dogsID, name: action.payload}]};
+            const dogsID = lastDog ? lastDog.id + 1 : 1;
+            return {...state, dogs: [...state.dogs, {id: dogsID, name: action.payload}]};
 
         case "DELETE_DOG":
-            const indexDog = state.dogs.indexOf(dog => dog.id === action.payload);
+            const indexDog = state.dogs.findIndex(dog => dog.id === action.payload);
             state.dogs.splice(indexDog, 1)
             return {...state};
 
@@ -37,45 +37,44 @@ const reducer = (state, action) => {
 
 const AnimalsReducer = () => {
 
-const [state, dispatch] = useReducer(reducer, {cats:[], dogs:[]}, (data)=> data);
-const {register, reset, handleSubmit} = useForm();
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []}, (data) => data);
+    const {register, reset, handleSubmit} = useForm();
 
-console.log(state.cats)
+    console.log(state.cats)
 
-    const createDog =  (data) => {
-
-       for (const dataKey in data) {
-           if (dataKey !== 'cats'){
-               dispatch({type:"ADD_DOG", payload: data[dataKey]})
-
-           }
-       }
-       reset()
+    const createDog = (data) => {
+        const {dogs} = data;
+        dispatch({type: "ADD_DOG", payload: dogs})
+        reset()
     }
 
-   const createCat =(data)=> {
+    const createCat = (data) => {
+        const {cats} = data;
+        dispatch({type: "ADD_CAT", payload: cats})
 
-       for (const dataKey in data) {
-           if (dataKey !== "dogs"){
-               dispatch({type:"ADD_CAT", payload: data[dataKey]})
-           }
-       }
-       reset()
-   }
+        console.log(cats)
+        reset()
+    }
 
     return (
         <div className={css.AnimalsReducer}>
-            <form  onSubmit={handleSubmit(createCat)}>
-                <input type="text" placeholder={"add cat"} {...register('cats')}/>
-                <button >Create Cat</button>
-                <Cats cats={state.cats} dispatch={dispatch}/>
-            </form>
+            <div>
+                <form onSubmit={handleSubmit(createCat)}>
+                    <input type="text" placeholder={"add cat"} {...register('cats')}/>
+                    <button>Create Cat</button>
+                </form>
 
-            <form onSubmit={handleSubmit(createDog)}>
-                <input type="text" placeholder={"add dog"} {...register("dogs")}/>
-                <button >Create Dog</button>
+                <Cats cats={state.cats} dispatch={dispatch}/>
+            </div>
+
+            <div>
+                <form onSubmit={handleSubmit(createDog)}>
+                    <input type="text" placeholder={"add dog"} {...register("dogs")}/>
+                    <button>Create Dog</button>
+                </form>
+
                 <Dogs dogs={state.dogs} dispatch={dispatch}/>
-            </form>
+            </div>
         </div>
     );
 };
